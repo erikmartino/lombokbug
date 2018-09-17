8fa3ab34b1141731d3e18420dfa7ca96b681e1c4
Erik Martino <martino@chainalysis.com>
2018-09-17 08:02:37 +0200

    Add final field

--- src/main/java/xylifyx/lomboktest/ValueObject.java
+++ src/main/java/xylifyx/lomboktest/ValueObject.java
@@ -3,8 +3,12 @@ package xylifyx.lomboktest;
 import lombok.Builder;
 import lombok.Value;
 
+/**
+ * Demo of Lombok in Netbeans
+ */
 @Value
 @Builder
 public class ValueObject {
-    
+
+    private final int value;
 }
